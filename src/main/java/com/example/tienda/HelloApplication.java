6f5c9d70465d546922f0ba65e03141f7707a4b2f
generico2600 <elgenerico2600@gmail.com>
2024-11-18 19:52:44 -0500

    Prueba materialfx

--- src/main/java/com/example/tienda/HelloApplication.java
+++ src/main/java/com/example/tienda/HelloApplication.java
@@ -1,5 +1,8 @@
 package com.example.tienda;
 
+import io.github.palexdev.materialfx.theming.JavaFXThemes;
+import io.github.palexdev.materialfx.theming.MaterialFXStylesheets;
+import io.github.palexdev.materialfx.theming.UserAgentBuilder;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,8 +13,15 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        UserAgentBuilder.builder()
+                .themes(JavaFXThemes.MODENA)
+                .themes(MaterialFXStylesheets.forAssemble(true))
+                .setDeploy(true)
+                .setResolveAssets(true)
+                .build()
+                .setGlobal();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-signup.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.tienda {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires MaterialFX;
 
     opens com.example.tienda to javafx.fxml;
     exports com.example.tienda;
