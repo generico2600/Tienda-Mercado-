749856fdd62565bb898c76cf23672845375f4473
generico2600 <elgenerico2600@gmail.com>
2024-12-03 14:00:06 -0500

    error de tipeo

--- src/main/java/com/example/tienda/Constants.java
+++ src/main/java/com/example/tienda/Constants.java
@@ -7,6 +7,6 @@ public class Constants {
     public static final String IMAGE_STORAGE_PATH = STORAGE_PATH + "img/";
     public static final String CART_FILE_PREFIX = STORAGE_PATH + "carrito_";
     public static final String HISTORY_FILE_PREFIX = STORAGE_PATH + "historial_";
-    public static final String WISHLIST_FILE_PREFIX = STORAGE_PATH + "historial_";
+    public static final String WISHLIST_FILE_PREFIX = STORAGE_PATH + "deseados_";
     public static final String FILE_SUFFIX = ".txt";
 }
